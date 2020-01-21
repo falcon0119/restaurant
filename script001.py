12fdcccc35af19bc7d752fad9c38acfe36c87067
zen4040 <59008143+zen4040@users.noreply.github.com>
2020-01-21 11:49:44 +0900

    Update script001.py

--- script001.py
+++ script001.py
@@ -12,6 +12,14 @@ def act1():
 
     foods = [food1, food2, food3, food4, food5, food6]
     
+    dressing1 = Dressing('Classic Caesar',0,73)
+    dressing2 = Dressing('Onion Soy',0,107)
+    dressing3 = Dressing('Creamy Seasame',0,67)
+    dressing4 = Dressing('Honey Mustard',0,127)
+    dressing5 = Dressing('Balsamic Vinaigrette',0,127)
+
+    dressings = [dressing1, dressing2, dressing3, dressing4, dressing5]
+    
     drink1 = Drink('Classic Caesar',0,73)
     drink2 = Drink('Onion Soy',0,107)
     drink3 = Drink('Creamy Seasame',0,67)
@@ -24,8 +32,14 @@ def act1():
     for food in foods:
         print(str(index) + '. ' + food.info())
         index += 1
-
+        
     print('Dressing Menu')
+    index = 0
+    for drink in dressings:
+        print(str(index) + '. ' + dressing.info())
+        index += 1
+
+    print('Drink Menu')
     index = 0
     for drink in drinks:
         print(str(index) + '. ' + drink.info())
@@ -35,6 +49,9 @@ def act1():
 
     food_order = int(input('Please select a number from the food menu: '))
     selected_food = foods[food_order]
+    
+    dressing_order = int(input('Please select a number from the dressing menu: '))
+    selected_dressing = dressings[dressing_order]
 
     drink_order = int(input('Please select a number from the drink menu: '))
     selected_drink = drinks[drink_order]
