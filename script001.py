a78ff3051e98ac83ca733ca22397f3c9f81eb16a
zen4040 <59008143+zen4040@users.noreply.github.com>
2020-01-21 11:44:38 +0900

    Update script001.py

--- script001.py
+++ script001.py
@@ -8,21 +8,24 @@ def act1():
     food3 = Food("Grilled Chicken & Egg Salad", 980, 262)
     food4 = Food('Prosciutto & Mimolette Cheese Salad', 980, 139)
     food5 = Food('Garlic Shrimp Salad', 1280, 113)
+    food6 = Food('Skip', 0, 0)
 
-    foods = [food1, food2, food3, food4, food5]
+    foods = [food1, food2, food3, food4, food5, food6]
+    
     drink1 = Drink('Classic Caesar',0,73)
     drink2 = Drink('Onion Soy',0,107)
     drink3 = Drink('Creamy Seasame',0,67)
+    drink4 = Drink('Skip',0, 0)
 
-    drinks = [drink1, drink2, drink3]
+    drinks = [drink1, drink2, drink3, drink4]
     
-    print('サラダメニュー')
+    print('Salad Menu')
     index = 0
     for food in foods:
         print(str(index) + '. ' + food.info())
         index += 1
 
-    print('ドレッシングメニュー')
+    print('Dressing Menu')
     index = 0
     for drink in drinks:
         print(str(index) + '. ' + drink.info())
@@ -30,10 +33,10 @@ def act1():
 
     print('--------------------')
 
-    food_order = int(input('食べ物の番号を選択してください: '))
+    food_order = int(input('Please select a number from the food menu: '))
     selected_food = foods[food_order]
 
-    drink_order = int(input('ドレッシングの番号を選択してください: '))
+    drink_order = int(input('Please select a number from the drink menu: '))
     selected_drink = drinks[drink_order]
 
 # コンソールから入力を受け取り、変数countに代入してください
@@ -43,4 +46,4 @@ def act1():
     result = selected_food.get_total_price(count) + selected_drink.get_total_price(count)
 
 # 「合計は〇〇円です」となるように出力してください
-    print('合計は' + str(result) + '円です')
+    print('Your total is ' + str(result) + ' Yen')
