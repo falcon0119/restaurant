43595eb5ab1fdad52dc308720f8ef0f48f46ed6d
sophia1814 <59009969+sophia1814@users.noreply.github.com>
2020-01-21 18:11:32 +0900

    Update script004.py

--- script004.py
+++ script004.py
@@ -18,13 +18,13 @@ def act4():
 
     drinks= [drink1, drink2]
     
-    print('フードメニュー')
+    print('FOOD MENU')
     index = 0
     for food in foods:
         print(str(index) + '. ' + food.info())
         index += 1
 
-    print('ドリンクメニュー')
+    print('DRINK MENU')
     index = 0
     for drink in drinks:
         print(str(index) + '. ' + drink.info())
@@ -45,4 +45,4 @@ def act4():
     result = selected_food.get_total_price(count) + selected_drink.get_total_price(count)
 
 # 「合計は〇〇円です」となるように出力してください
-    print('合計は' + str(result) + '円です')
+    print('Your total is ' + str(result) + ' Yen')
