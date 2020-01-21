bf137a5d33dccde18ba460abe67d78d6911cc83d
sophia1814 <59009969+sophia1814@users.noreply.github.com>
2020-01-21 18:03:37 +0900

    Update food.py

--- food.py
+++ food.py
@@ -8,5 +8,4 @@ class Food(MenuItem):
     def info(self):
         return self.name + ': ¥' + str(self.price) + ' (' + str(self.calorie) + 'kcal)'
     
-    def calorie_info(self):
-        print(str(self.calorie) + 'kcalです')
+   
