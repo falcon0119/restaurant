8122869a3c1c5c1bd2aa9f8bf13ccdd407827510
zen4040 <59008143+zen4040@users.noreply.github.com>
2020-01-21 23:08:28 +0900

    Update script004.py

--- script004.py
+++ script004.py
@@ -10,13 +10,13 @@ def act4():
     food5 = Food('Bulgogi Set Menu', 1000, 890)
     food6 = Food('Skip', 0, 0)
              
-    foods = [food1, food2, food3, food4, food5]
+    foods = [food1, food2, food3, food4, food5, food6]
 
     drink1 = Drink('Mineral Water', 200, 0)
     drink2 = Drink('Oolong Tea', 200, 0)
     drink3 = Drink('Skip',0 ,0) 
 
-    drinks= [drink1, drink2]
+    drinks= [drink1, drink2, drink3]
     
     print()
     print('***** FOOD MENU *****')
@@ -34,22 +34,46 @@ def act4():
 
     print('--------------------')
 
-    food_order = int(input('Please select a number from the food menu: '))
-    selected_food = foods[food_order]
+# if choose invalid numbers 
 
-    drink_order = int(input('Please select a number from the drink menu: '))
-    selected_drink = drinks[drink_order]
+    while True:
+        food_order = input('Please select a number from the food menu: ')
+
+        if food_order == '0' or food_order == '1' or food_order == '2' or food_order == '3' or food_order == '4' or food_order == '5' or food_order == '6':
+            selected_food = foods[int(food_order)]
+
+        else:
+            print("Please insert a valid number.")
+            print()
+            continue
+        
+        break
+
+    while True:
+        drink_order = input('Please select a number from the drink menu: ')
+
+        if drink_order == '0' or drink_order == '1' or drink_order == '2' or drink_order == '3':
+            selected_drink = drinks[int(drink_order)]
+
+        else:
+            print("Please insert a valid number.")
+            print()
+            continue
+        
+        break
+     
 
     count = int(input('How many sets would you like to purchase？(10% off when you purchase 3 sets): '))
 
     result = selected_food.get_total_price(count) + selected_drink.get_total_price(count)
 
+    #print total
+    
     print()
     print('----------------------------------------------------------')
     print('Here is your total order:')
     print()
     print(str(selected_food.name) + ' ¥' + str(selected_food.price) + ' x ' + str(count))
-    print(str(selected_dressing.name) + ' ¥' + str(selected_dressing.price) + ' x ' + str(count))
     print(str(selected_drink.name) + ' ¥' + str(selected_drink.price) + ' x ' + str(count))
     print('----------------------------------------------------------')
     print('Your total is ' + str(result) + ' Yen')
