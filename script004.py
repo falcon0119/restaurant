a109688b033ee5c42ae48801ba0bd3a43b21fa0b
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2020-01-22 09:58:20 +0900

    Update script004.py

--- script004.py
+++ script004.py
@@ -75,6 +75,13 @@ def act4():
     print()
     print(str(selected_food.name) + ' ¥' + str(selected_food.price) + ' x ' + str(count))
     print(str(selected_drink.name) + ' ¥' + str(selected_drink.price) + ' x ' + str(count))
+    
+    if count>=3:
+        print ('10% discount')
+
+    else:
+        print('    ')
+
     print('----------------------------------------------------------')
     print('Your total is ' + str(result) + ' Yen')
     print()
